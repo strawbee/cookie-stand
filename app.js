e718dca621952713ef50133ec9a7f9ef199dc893
strawbee <yuejiao.hou@gmail.com>
2017-09-27 14:33:35 -0700

    Got table footer to update when adding new store

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 var i;
 var arrayOfHoursOpen = [
-  '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'
+  '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'
 ];
 
 // Constructor Function for Pat's Salmon Cookies stores
-function PatsSalmonCookiesStore(name, minCustPerHr, maxCustPerHr, avgCookiesPerSale) {
+function PatsSalmonCookiesStore(id, name, minCustPerHr, maxCustPerHr, avgCookiesPerSale) {
   this.name = name;
   this.minCustPerHr = minCustPerHr;
   this.maxCustPerHr = maxCustPerHr;
@@ -14,11 +14,11 @@ function PatsSalmonCookiesStore(name, minCustPerHr, maxCustPerHr, avgCookiesPerS
   this.cookiesSoldPerHr = [];
 }
 
-var locFirstAndPike = new PatsSalmonCookiesStore('First and Pike', 23, 65, 6.3);
-var locSeaTacAirport = new PatsSalmonCookiesStore('SeaTac Airport', 3, 24, 1.2);
-var locSeattleCenter = new PatsSalmonCookiesStore('Seattle Center', 11, 38, 3.7);
-var locCapitolHill = new PatsSalmonCookiesStore('Capitol Hill', 20, 38, 2.3);
-var locAlki = new PatsSalmonCookiesStore('Alki', 2, 16, 4.6);
+var locFirstAndPike = new PatsSalmonCookiesStore('locFirstAndPike', 'First and Pike', 23, 65, 6.3);
+var locSeaTacAirport = new PatsSalmonCookiesStore('locSeaTacAirport', 'SeaTac Airport', 3, 24, 1.2);
+var locSeattleCenter = new PatsSalmonCookiesStore('locSeattleCenter', 'Seattle Center', 11, 38, 3.7);
+var locCapitolHill = new PatsSalmonCookiesStore('locCapitolHill', 'Capitol Hill', 20, 38, 2.3);
+var locAlki = new PatsSalmonCookiesStore('locAlki', 'Alki', 2, 16, 4.6);
 
 var arrayOfStoreLocations = [
   locFirstAndPike,
@@ -63,31 +63,8 @@ PatsSalmonCookiesStore.prototype.renderCookiesSold = function() {
   thTdPos.appendChild(tdEl);
 };
 
-// Generate Table Head
-(function () {
-  var thEl, thTextNode, thPos, i;
-  for (i in arrayOfHoursOpen) {
-    thEl = document.createElement('th');
-    thTextNode = document.createTextNode(arrayOfHoursOpen[i]);
-    thEl.appendChild(thTextNode);
-    thPos = document.querySelector('thead').lastChild;
-    thPos.appendChild(thEl);
-  }
-
-  thEl = document.createElement('th');
-  thTextNode = document.createTextNode('Total');
-  thEl.appendChild(thTextNode);
-  thPos.appendChild(thEl);
-})();
-
-// Generate Table Body - always call table body before the table footer
-
-for (i in arrayOfStoreLocations) {
-  arrayOfStoreLocations[i].renderCookiesSold();
-}
-
-// Generate Table Footer
-(function () {
+// Function to Render Table Footer
+function renderTableFooter() {
   var thEl, thTextNode, thTdPos, tdEl, tdTextNode, i, j;
   var hourTotal = 0;
   var total = 0;
@@ -114,4 +91,75 @@ for (i in arrayOfStoreLocations) {
   tdTextNode = document.createTextNode(total);
   tdEl.appendChild(tdTextNode);
   thTdPos.appendChild(tdEl);
+}
+
+// Function to Remove Table Footer
+function removeTableFooter() {
+  var tableFooter, firstTr;
+  tableFooter = document.querySelector('tfoot');
+  firstTr = tableFooter.firstChild;
+  while (firstTr.firstChild) {
+    firstTr.removeChild(firstTr.firstChild);
+  }
+}
+
+// Generate Table Head
+(function () {
+  var thEl, thTextNode, thPos, i;
+  for (i in arrayOfHoursOpen) {
+    thEl = document.createElement('th');
+    thTextNode = document.createTextNode(arrayOfHoursOpen[i]);
+    thEl.appendChild(thTextNode);
+    thPos = document.querySelector('thead').lastChild;
+    thPos.appendChild(thEl);
+  }
+
+  thEl = document.createElement('th');
+  thTextNode = document.createTextNode('Total');
+  thEl.appendChild(thTextNode);
+  thPos.appendChild(thEl);
 })();
+
+// Generate Table Body - always call table body before the table footer
+
+for (i in arrayOfStoreLocations) {
+  arrayOfStoreLocations[i].renderCookiesSold();
+}
+renderTableFooter();
+
+
+// Function to Add New Store
+function addLocation(event) {
+  event.preventDefault();
+  var locId = event.target.locId.value;
+  var locName = event.target.locName.value;
+  var minCustRaw = event.target.minCust.value;
+  var minCust = Math.round(minCustRaw);
+  var maxCustRaw = event.target.maxCust.value;
+  var maxCust = Math.round(maxCustRaw);
+  var avgCookiesRaw = event.target.avgCookies.value;
+  var avgCookies = Math.round(avgCookiesRaw);
+  var formValidation = document.getElementById('formValidation');
+
+  // Form Validation
+  if (!locId || !locName || !minCustRaw || !maxCustRaw || !avgCookiesRaw) {
+    formValidation.textContent = 'Please fill in all fields.';
+  }
+  else if (isNaN(minCustRaw) || isNaN(maxCustRaw) || isNaN(avgCookiesRaw)) {
+    formValidation.textContent = 'Minimum customers, maximum custoners, and average number of cookies should be numbers.';
+  }
+
+
+  else {
+    // Add New Location to Table
+    var newLocation = new PatsSalmonCookiesStore(locId, locName, minCust, maxCust, avgCookies);
+    arrayOfStoreLocations.push(newLocation);
+    newLocation.renderCookiesSold();
+
+    removeTableFooter();
+    renderTableFooter();
+  }
+}
+
+var addLoc = document.getElementById('addLocation');
+addLoc.addEventListener('submit', addLocation);
