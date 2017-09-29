20e1eed7f1bd916ff1d89941391517c88af9ffbb
strawbee <yuejiao.hou@gmail.com>
2017-09-28 23:35:18 -0700

    removed some variables

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ function PatsSalmonCookiesStore(name, minCustPerHr, maxCustPerHr, avgCookiesPerS
 
 PatsSalmonCookiesStore.hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
 
-var locFirstAndPike = new PatsSalmonCookiesStore('First and Pike', 23, 65, 6.3);
-var locSeaTacAirport = new PatsSalmonCookiesStore('SeaTac Airport', 3, 24, 1.2);
-var locSeattleCenter = new PatsSalmonCookiesStore('Seattle Center', 11, 38, 3.7);
-var locCapitolHill = new PatsSalmonCookiesStore('Capitol Hill', 20, 38, 2.3);
-var locAlki = new PatsSalmonCookiesStore('Alki', 2, 16, 4.6);
+new PatsSalmonCookiesStore('First and Pike', 23, 65, 6.3);
+new PatsSalmonCookiesStore('SeaTac Airport', 3, 24, 1.2);
+new PatsSalmonCookiesStore('Seattle Center', 11, 38, 3.7);
+new PatsSalmonCookiesStore('Capitol Hill', 20, 38, 2.3);
+new PatsSalmonCookiesStore('Alki', 2, 16, 4.6);
 
 PatsSalmonCookiesStore.prototype.randomNumCustPerHr = function() {
   return Math.floor(Math.random() * (this.maxCustPerHr - this.minCustPerHr + 1) + this.minCustPerHr);
